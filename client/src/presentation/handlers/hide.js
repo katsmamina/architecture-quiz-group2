682120eec08afd18a04aa9f5ccf7f2c3dee4a0f1
Walter Alvarado <yovaemperor@gmail.com>
2021-09-16 10:31:27 +0200

    Update hide.js
    change "style.display" to "classlist.add"

--- client/src/presentation/handlers/hide.js
+++ client/src/presentation/handlers/hide.js
@@ -1,5 +1,5 @@
 export const hideWelcomeMessage = (e) => {
     // hide the name of the quiz, the description and the start button
     e.preventDefault();
-    document.getElementById('welcome-message').style.display = "none";
+    document.getElementById('welcome-message').classList.add('hide');
 }

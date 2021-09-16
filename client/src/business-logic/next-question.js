91eead20b68ea89046a351517ab6f2c67ec8c9d0
Walter Alvarado <yovaemperor@gmail.com>
2021-09-16 09:52:11 +0200

    add business logic of "next question"
    start of the business logic behind "next question" functionality

---
+++ client/src/business-logic/next-question.js
@@ -0,0 +1,5 @@
+import { find } from '../data-access/find'
+
+export const nextQuestion = (e) => {
+    
+}
---
+++ client/src/business-logic/next-question.spec.js
@@ -0,0 +1,5 @@
+import { nextQuestion } from './next-question.js'
+
+describe('nextQuestion: adds next question in the array "JSON"', () =>{
+
+});

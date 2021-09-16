4c1de918cdb4b6f271677ce6f41357ad0ac903e2
deepa@hyf123 <deepamlavil@gmail.com>
2021-09-16 18:22:45 +0200

    display first question and answers

---
+++ client/src/presentation/handlers/display.js
@@ -0,0 +1,16 @@
+import { accessQuestion } from '../../business-logic/get-question.js';
+import { accessAnswers } from '../../business-logic/get-answer.js';
+
+export const displayFirstQuestionAndAnswers = (e) => {
+  // display first question and answers and change innerHTML of start button
+  e.preventDefault();
+  document.getElementById('container-quiz').classList.remove('hide');
+  document.getElementById('btn').innerHTML = 'Next';
+  const question = accessQuestion(0);
+  document.getElementById('quiz-question').innerText = question;
+  const answers = accessAnswers(0);
+  document.getElementById('p-answer-1').innerText = answers[0].text;
+  document.getElementById('p-answer-2').innerText = answers[1].text;
+  document.getElementById('p-answer-3').innerText = answers[2].text;
+  document.getElementById('p-answer-4').innerText = answers[3].text;
+};
--- client/src/presentation/init/index.js
+++ client/src/presentation/init/index.js
@@ -5,11 +5,5 @@ import { loadListener } from '../listeners/load.js';
 
 import { startQuiz } from '../listeners/start.js';
 
-import { accessQuestion } from '../../business-logic/get-question.js';
-import { accessAnswers } from '../../business-logic/get-answer.js';
-
 // keep this listener call!  you will need it
 loadListener();
-
-accessQuestion();
-accessAnswers();

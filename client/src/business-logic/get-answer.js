d7b8210647e92f5d5ead1fb2988ca01b1d55c215
danielhalasz <halaszdaniel@gmail.com>
2021-09-16 10:13:50 +0200

    added get-answer.js business logic

--- client/src/business-logic/get-answer.js
+++ client/src/business-logic/get-answer.js
@@ -1,9 +1,9 @@
 import { findAll } from '../data-access/find-all.js';
 
-export const accessQuestion = () => {
+export const accessAnswers = () => {
   const data = findAll();
   const questionsObject = data[0];
   const questionsArray = Object.values(questionsObject);
-  console.log(questionsArray[1][0].question);
-  // return questionsArray[1][i].question;
+  console.log(questionsArray[1][0].answers[1].text);
+  // return questionsArray[1][i].answers;
 };

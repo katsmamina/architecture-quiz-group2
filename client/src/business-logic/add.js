de7afe291b3ff5a101039d556eb3c8c68e9ca627
Walter Alvarado <yovaemperor@gmail.com>
2021-09-18 13:08:35 +0200

    Create add.js
    Create add.js this should work to iterate through the array of answers

---
+++ client/src/business-logic/add.js
@@ -0,0 +1,10 @@
+import { findAll } from '../data-access/find-all.js';
+
+export const accessAnswers = (i = 0) => {
+  const data = findAll();
+  const questionsObject = data[0];
+  const questionsArray = Object.values(questionsObject);
+  const answerArray = questionsArray[1][i].answers;
+    for (let i = 0; i < 9; i++); // increment +1 each time 
+  return answerArray;
+};
